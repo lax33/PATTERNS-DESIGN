91fe9ec411800bd73cff283239771fd7469a9425
lax33 <lax33@tut.by>
2022-05-14 16:10:23 +0300

    add ClassDiagramm

--- ChainOfResponsibility/ProgramCOR.cs
+++ ChainOfResponsibility/ProgramCOR.cs
@@ -12,7 +12,7 @@ namespace ChainOfResponsibility
             var dog = new DogHandler();
 
             monkey.SetNext(squirrel).SetNext(dog);
-
+            monkey.Handle("Banana");
             // Клиент должен иметь возможность отправлять запрос любому
             // обработчику, а не только первому в цепочке.
             Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
@@ -21,6 +21,8 @@ namespace ChainOfResponsibility
 
             Console.WriteLine("Subchain: Squirrel > Dog\n");
             Client.ClientCode(squirrel);
+
+            Console.ReadLine();
         }
     }
 }
