3282f93c7ca443de9a3baf8a99bdf546bbd6aa8f
lax33 <lax33@tut.by>
2022-06-03 18:00:06 +0300

    1

---
+++ Command/ProgramCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Command
+{
+    internal class ProgramCommand
+    {
+        private static void Main(string[] args)
+        {
+            //Команда — это поведенческий паттерн проектирования,
+            //который превращает запросы в объекты, позволяя
+            //передавать их как аргументы при вызове методов, ставить
+            //запросы в очередь, логировать их, а также поддерживать
+            //отмену операций.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
